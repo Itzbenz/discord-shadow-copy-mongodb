583a86cbc74ce4874ead7367bde357ea817d553d
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2023-04-21 21:12:10 +0700

    really

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [{
         name: "discord-shadow-copy-mongodb",
         script: "./index.js",
-        max_memory_restart: "100M",
+        max_memory_restart: "400M",
         autorestart: true,
         env: {
             NODE_ENV: "development",
