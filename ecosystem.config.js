1b7c9fb01a586a7503dc67321181e0d944a34552
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2023-04-21 18:44:13 +0700

    resilience

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
     apps : [{
         name: "discord-shadow-copy-mongodb",
         script: "./index.js",
+        max_memory_restart: "100M",
+        autorestart: true,
         env: {
             NODE_ENV: "development",
         },
